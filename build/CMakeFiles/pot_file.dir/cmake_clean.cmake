5702bbb230357a3dc8ca7d981dfc54ba09c37235
Zahidul Hossain <choyan.009@gmail.com>
2017-02-19 14:25:59 +0600

    changed the app category to office

--- build/CMakeFiles/pot_file.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pot_file"
-  "po/bookworm.pot"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pot_file.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
