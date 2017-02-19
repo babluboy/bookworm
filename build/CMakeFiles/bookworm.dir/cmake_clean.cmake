4e8f32b54c4e675097d598a35cdae45491234fc0
Zahidul Hossain <choyan.009@gmail.com>
2017-02-19 14:25:31 +0600

    changed the app category to office

---
+++ build/CMakeFiles/bookworm.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "src/bookworm.c"
+  "src/utils.c"
+  "src/constants.c"
+  "src/ePubReader.c"
+  "src/book.c"
+  "bookworm_valac.stamp"
+  "CMakeFiles/bookworm.dir/src/bookworm.c.o"
+  "CMakeFiles/bookworm.dir/src/utils.c.o"
+  "CMakeFiles/bookworm.dir/src/constants.c.o"
+  "CMakeFiles/bookworm.dir/src/ePubReader.c.o"
+  "CMakeFiles/bookworm.dir/src/book.c.o"
+  "bookworm.pdb"
+  "bookworm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bookworm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/pot_file.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pot_file"
+  "po/bookworm.pot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pot_file.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
